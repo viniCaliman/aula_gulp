489ef36df2d1367b1abaf42d77916efeb2ebb88c
Vinicius Paiva Caliman <vinicaliman@gmail.com>
2024-06-04 16:38:17 -0300

    fifth lesson: como compilar o sass via gulp e como criar um watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps');
+
+function compileSass(){
+    return gulp.src('./src/styles/main.scss')
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourceMaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'));
+}
 
 function print(callback) {
     setTimeout(() => {
@@ -19,3 +31,7 @@ function dizerTchau(){
 }
 
 exports.default = gulp.parallel(print, dizerOi);
+exports.sass = compileSass;
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(compileSass))
+}
